a83a7549f1429ad4d5e09ad5993830b7e83800cc
Raymond Ruibi Muthii <raymondmuthii@gmail.com>
2024-07-24 18:34:12 +0300

    scope lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your solution in this file!
+var customerName = 'bob';
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+function setBestCustomer() {
+  bestCustomer = 'not bob'; // Declaring a global variable
+}
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+const leastFavoriteCustomer = 'some initial value';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'new value';
+}
